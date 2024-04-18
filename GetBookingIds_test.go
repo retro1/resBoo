b73ab7941357a05b4b85e669efc1a8acaf65da0b
retro1 <stealth92@mail.ru>
2024-04-18 21:44:55 +0300

    Update GetBookingIds_test.go

--- GetBookingIds_test.go
+++ GetBookingIds_test.go
@@ -8,36 +8,28 @@ import (
 	"testing"
 )
 
-func TestGetBooking(t *testing.T) {
-	req, err := http.NewRequest("GET", "https://restful-booker.herokuapp.com/booking/1", nil)
-	if err != nil {
-		t.Error("Error Get request:", err)
-		return
-	}
-
-	req.Header.Add("Accept", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
+func TestGetBookId(t *testing.T) {
+	resp, err := http.Get("https://restful-booker.herokuapp.com/booking")
 	if resp.StatusCode != http.StatusOK {
-		t.Error("Error Get request:", resp.StatusCode)
+		t.Error("Error to HTTP request:", resp.StatusCode)
 		return
 	} else {
-		t.Log("Success:", resp.Proto, resp.Status)
+		t.Log("Success:", resp.Proto, resp.StatusCode)
 	}
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
+	resRead, err := io.ReadAll(resp.Body)
 	if err != nil {
-		t.Error("Error reading response:", err)
+		t.Error("Error Read response:", err)
 		return
 	}
-	var data map[string]interface{}
-	err = json.Unmarshal(body, &data)
+
+	var data []map[string]interface{}
+	err = json.Unmarshal([]byte(resRead), &data)
 	if err != nil {
-		t.Error("Error JSON unmarshal:", err)
+		t.Error("Error Unmarshal:", err)
 		return
 	}
-	jsonF, _ := json.MarshalIndent(data, "", "\t")
+	jsonF, err := json.MarshalIndent(data, "", "\t")
 	fmt.Printf("Received JSON:\n%s%s", string(jsonF), "\n")
 }
