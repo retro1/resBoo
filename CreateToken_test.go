5ab3d9b037493cbe6c29b8f5d09f032becfe1322
retro1 <stealth92@mail.ru>
2024-04-18 21:43:57 +0300

    Update CreateToken_test.go

--- CreateToken_test.go
+++ CreateToken_test.go
@@ -1,4 +1,4 @@
-package main
+package Test_Tasks
 
 import (
 	"bytes"
@@ -15,25 +15,29 @@ func TestAuth(t *testing.T) {
 		"password": "password123",
 	})
 	if err != nil {
-		fmt.Println("Error to JSON encode:", err)
+		t.Error("Error to JSON encode:", err)
+		return
 	}
 
 	req, err := http.Post("https://restful-booker.herokuapp.com/auth", "application/json", bytes.NewBuffer(reqBody))
 	if req.StatusCode != http.StatusOK {
-		fmt.Println("Error to HTTP request:", req.StatusCode)
+		t.Error("Error to HTTP request:", req.StatusCode)
+		return
 	} else {
-		fmt.Println("Success HTTP request:", req.Proto, req.Status)
+		t.Log("Success HTTP request:", req.Proto, req.Status)
 	}
 	defer req.Body.Close()
 
 	rep, err := io.ReadAll(req.Body)
 	if err != nil {
-		fmt.Println("Error Host reply:", err)
+		t.Error("Error Host reply:", err)
+		return
 	}
 	var data map[string]interface{}
 	err = json.Unmarshal(rep, &data)
 	if err != nil {
-		fmt.Println("Error JSON unmarshal:", err)
+		t.Error("Error JSON unmarshal:", err)
+		return
 	}
 	jsonForm, err := json.MarshalIndent(data, "", "\t")
 	fmt.Printf("Received JSON:\n%s%s", string(jsonForm), "\n")
